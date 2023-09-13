772d9db406a67493ed6bdc545a97e1190e1af2b7
prospero <g0lden_vo1d@protonmail.com>
2023-09-13 09:24:32 +0200

    update

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+define('ROOT', __DIR__ . "/..");
+define('APP_ENVIRONMENT', "development");
+
+if (APP_ENVIRONMENT === "production") {
+    require_once(ROOT . "/utils/production.php");
+} elseif (APP_ENVIRONMENT === "development") {
+    require_once(ROOT . "/utils/development.php");
+}
+
+require_once(ROOT . "/vendor/autoload.php");

9e717c47261853312e4a28bd8f9429ef54aeaaa5
prospero <g0lden_vo1d@protonmail.com>
2023-09-24 10:24:35 +0200

    sync

---
+++ templates/views/error-page.view.php
@@ -0,0 +1,10 @@
+<?php require_once(PARTIALS . "/head.view.php"); ?>
+<?php require_once(PARTIALS . "/header.view.php"); ?>
+
+<div>
+    <p>Error occured. Please try again later.</p>
+</div>
+
+<?php require_once(PARTIALS . "/scripts.view.php"); ?>
+<?php require_once(PARTIALS . "/footer.view.php"); ?>
+<?php require_once(PARTIALS . "/pageend.view.php"); ?>
